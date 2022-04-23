049357823a098f073a3371ec63a2e2f13c47cde4
sds90 <smorodinov.denis@gmail.com>
2022-04-23 23:18:48 +0300

    Поправил gitignore

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+let gulp = require('gulp');
+let browserSync = require('browser-sync').create();
+let reload = browserSync.reload;
+let runSequence = require('run-sequence');
+
+function defaultTask(cb) {
+    gulp.task('pug', function () {
+        return gulp.src('sources/*.pug')
+                .pipe(pug({pretty: true}))
+                .pipe(gulp.dest(''))
+        });
+        
+    gulp.task('watch', function(){
+        $.watch('sources/**/*.pug ', function(event, cb) {
+                return runSequence('pug', reload);
+            });
+    });
+
+    cb();
+}
+
+exports.default = defaultTask;
