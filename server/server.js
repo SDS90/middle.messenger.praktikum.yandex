355de115ea03bdadd6d2c72f74d619a209d185a5
sds90 <smorodinov.denis@gmail.com>
2022-04-23 14:15:56 +0300

    Добавил переходы по страницам в приложении

--- server/server.js
+++ server/server.js
@@ -3,7 +3,31 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.set('views', './sources');
+app.set('view engine', 'pug');
+
 app.use(express.static('./sources/'));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Вход' });
+});
+
+app.get('/chat/', (req, res) => {
+  res.render('chat', { title: 'Чат' });
+});
+
+app.get('/reg-form/', (req, res) => {
+  res.render('reg-form', { title: 'Регистрация' });
+});
+
+app.get('/error/', (req, res) => {
+  res.render('error', { title: 'Ошибка' });
+});
+
+app.get('/profile/', (req, res) => {
+  res.render('profile', { title: 'Мой рофиль' });
+});
+
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
